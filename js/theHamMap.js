966fe478a2c1bd9e511db6f30825d92e850a9127
Natali <natalinn@i.ua>
2017-09-12 19:25:40 +0300

    5

--- js/theHamMap.js
+++ js/theHamMap.js
@@ -12,7 +12,7 @@
             mapTypeControl: false,
             scaleControl: false,
             draggable: false,
-            center: new google.maps.LatLng(49.832584, 23.993503099999998),
+            center: new google.maps.LatLng(49.82823871, 23.95774841),
 
             styles: [{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#ffcc00"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#bfc0c1"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"labels.text","stylers":[{"color":"#336699"},{"weight":"0.01"}]},{"featureType":"administrative.country","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":"1.71"}]},{"featureType":"administrative.country","elementType":"labels.icon","stylers":[{"hue":"#ff0000"}]},{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"visibility":"on"}]},{"featureType":"administrative.province","elementType":"labels.text.fill","stylers":[{"weight":"0.89"},{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"color":"#ffcc00"}]},{"featureType":"administrative.locality","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"hue":"#ff6600"}]},{"featureType":"administrative.locality","elementType":"labels.text","stylers":[{"visibility":"on"},{"weight":"0.01"},{"lightness":"0"},{"color":"#606060"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#336699"},{"visibility":"on"}]}]
         };
@@ -22,13 +22,13 @@
         var map = new google.maps.Map(mapElement, mapOptions);
 
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(49.832584, 23.993503099999998),
+            position: new google.maps.LatLng(49.82823871, 23.95774841),
             map: map,
-            icon: 'img/map-marker.png',
-            title: "QALight"
+            icon: 'img/logo-icon.png',
+            title: "TheHam"
         });
 
-        var contentString = 'Hello world! QALight is here!';
+        var contentString = 'Hello world! TheHam is here!';
 
         var infoWindow = new google.maps.InfoWindow({
             content: contentString
